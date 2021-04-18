f7b2eae75b0a0e07d075ba60d69d6adcec8a0e4a
suryaharahap <suryaharahap18@gmail.com>
2021-04-18 17:56:34 +0700

    learned about variables, simple data types, and string interpolation

---
+++ MyPlayground-Day1.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+var str = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+var pi = 3.141
+var awesome = true
+
+
+//String interpolation
+var score = 18
+var str3 = "Your score was \(score)"
+var result = "The test results are here: \(str3) "
+
+//Constants
+let taylor = "Swift"
+
+//Type anotations
+let str4 = "hello playground"
+let album: String = "Reputation"
+let year: Int = 1999
+let height: Double = 1.78
+let taylorRocks: Bool = true
